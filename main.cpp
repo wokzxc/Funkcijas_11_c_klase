c6ade4b40de2914dfe763addd9bd95c077530e4e
edgarszogots <100197230+edgarszogots@users.noreply.github.com>
2022-03-04 07:57:52 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int process (int x, int y, int & max, double & avg) 
+{
+
+}
+int main() {
+	int a,b,liel;
+	double vid;
+	cout << "Ievadiet skaitļus:\n";
+	cin >> a >> b;
+	int diff=process(a,b,liel,vid);
+	cout << "Lielākais: "
+		<< liel << endl;
+	cout << "Vidējais: "
+		<< vid	<< endl;
+	cout << "Starpība: "
+		<< diff << endl;
+	return 0;
+}
