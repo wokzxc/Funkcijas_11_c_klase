0f0f26a17c702df24aa82aa3d5aee803e7ff25d9
wokzxc <100773702+wokzxc@users.noreply.github.com>
2022-03-04 10:35:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 using namespace std;
-int process (int x, int y, int & max, double & avg) 
+int process (int x, int y, int & max,int diff, double & avg) 
 {
-
+if(x>y) {
+  diff = x-y;
+  max = x;
+  avg= (x+y)/2;
+  }
+  else{
+  max = y;
+  diff = y-x;
+  avg= (x+y)/2;
+  
+  }
 }
 int main() {
-	int a,b,liel;
-	double vid;
+	int x,y,max;
+	double avg;
 	cout << "Ievadiet skaitļus:\n";
-	cin >> a >> b;
-	int diff=process(a,b,liel,vid);
+	cin >> x >> y;
+	int diff=process(x,y,max,diff,avg);
 	cout << "Lielākais: "
-		<< liel << endl;
+		<< max << endl;
 	cout << "Vidējais: "
-		<< vid	<< endl;
+		<< avg	<< endl;
 	cout << "Starpība: "
 		<< diff << endl;
 	return 0;
